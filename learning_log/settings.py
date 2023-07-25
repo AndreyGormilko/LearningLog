acab2723d735537fb93702fcced3312a583920b2
Andrey <gormilkoandrey@gmail.com>
2023-07-26 01:03:00 +0300

    4.2

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&k5^+0py!zm$@u_x-5&$--%l8t3tom0do9d_cjc$_dbcze$xb%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
